725a7c9206f96ed903c782e5b6bce49b376ecf9c
canoriz <83388275+canoriz@users.noreply.github.com>
2026-04-20 19:19:51 +0800

    remove old cache

--- src/cache.rs
+++ src/cache.rs
@@ -1,22 +1,9 @@
-mod buffer_storage;
-mod global;
-mod piece;
 pub mod simple_buffer;
 use std::sync::{Arc, Mutex};
-
-pub(crate) use buffer_storage::BufStorage;
-pub(crate) use global::{wake_next_waiting_alloc, AllocReq};
-pub(crate) use global::{AllocErr, PieceBuf, PieceBufPool, PieceKey};
-pub(crate) use piece::{AbortErr, ArcCache, AsyncAbortRead, GetRefErr, Ref, SubAbortHandle};
 
 use crate::backfile::BackFile;
 
 type MutexBackFile = Arc<Mutex<BackFile>>;
-
-const DROPPED: u32 = 0b01;
-const DONE: u32 = 0b010; // TODO: really need this?
-const WAITING: u32 = 0;
-const WAKING: u32 = 0b100;
 
 #[cfg(test)]
 mod test {}
